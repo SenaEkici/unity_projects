0749d4bd98984a7e4575aea48df4e25f2a035a25
Sena Nur Ekici <45152078+SenaEkici@users.noreply.github.com>
2021-08-20 18:25:54 +0300

    Add files via upload

---
+++ adxl345.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using System.IO;
+
+
+
+public class adxl345 : MonoBehaviour
+{
+    //seri haberleşme kodları acıklama satırı haline getirdim.
+   
+    
+   
+    SerialPort serial = new SerialPort("COM7", 9600);
+    private int pitch;
+    private int roll;
+    
+    // Use this for initialization
+    void Start()
+    {
+    serial.Open();
+    serial.ReadTimeout = 100;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sd card reading
+      // StreamReader reader = new StreamReader(@"C:\sdcard\YENI123.txt");
+       // roll = int.Parse(reader.ReadLine());
+      //  pitch = int.Parse(reader.ReadLine());
+      //  MoveObject(pitch, 0, roll);
+        if (serial.IsOpen)
+            {
+                try
+                {
+
+                    roll = int.Parse(serial.ReadLine());
+                    pitch = int.Parse(serial.ReadLine());
+                    MoveObject(pitch, 0,roll);
+
+
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+            }
+         
+
+    }
+    void MoveObject(int xg,int yg,int zg)
+    {
+        transform.localEulerAngles = new Vector3(xg, yg, zg);
+    }
+    
+      
+       
+
+ 
+    
+}
