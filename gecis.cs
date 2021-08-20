b05837ea2999d648f0fb5022ad06425becf909fc
Sena Nur Ekici <45152078+SenaEkici@users.noreply.github.com>
2021-08-20 18:27:29 +0300

    Add files via upload

---
+++ gecis.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gecis : MonoBehaviour {
+    void sahneGecis(int SahneID)
+    {
+        SceneManager.LoadScene(SahneID);
+    }
+	
+}
